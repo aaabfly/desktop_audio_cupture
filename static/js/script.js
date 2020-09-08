5a63bb09dfd5135e2c0c73ec75a74bf3078ba30a
aaabfly <kd1274038@st.kobedenshi.ac.jp>
2020-09-08 15:21:16 +0900

    update

--- static/js/script.js
+++ static/js/script.js
@@ -1,19 +1,19 @@
 navigator.mediaDevices.getUserMedia({
     audio:{
-          mandatory: {
-                  chromeMediaSource: 'desktop',
-                  chromeMediaSourceId: streamId
-                }
+        mandatory: {
+            chromeMediaSource: 'desktop',
+            chromeMediaSourceId: streamId
+        }
         },
     video: {
-          mandatory: {
-                  chromeMediaSource: 'desktop',
-                  chromeMediaSourceId: streamId
-                }
+        mandatory: {
+            chromeMediaSource: 'desktop',
+            chromeMediaSourceId: streamId
         }
+    }
 }).then(stream => {
-      stream.getVideoTracks().forEach(track => stream.removeTrack(track)); // videoトラック削除
-      video.srcObject = stream;
+    stream.getVideoTracks().forEach(track => stream.removeTrack(track)); // videoトラック削除
+    video.srcObject = stream;
 }).catch(err => {
-   // error
-     });
+    // error
+});
